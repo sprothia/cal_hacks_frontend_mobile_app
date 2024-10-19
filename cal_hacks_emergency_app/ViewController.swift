fe786f306bf76c25fe59bb5a47d9b00685fb404d
Siddharth Prothia <sprothia935@gmail.com>
2024-10-18 21:12:18 -0700

    Initial Commit

---
+++ cal_hacks_emergency_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cal_hacks_emergency_app
+//
+//  Created by Siddharth Prothia on 10/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
